4a4c3ee114b448ed88cbc6a17928db2c3485dce0
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-09-25 00:10:53 +0100

    update: task controller

---
+++ src/main/java/com/example/chatbotapi/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.example.chatbotapi.controller;
+
+import com.example.chatbotapi.dto.Task;
+import com.example.chatbotapi.service.OptimusService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final OptimusService optimusService;
+
+    public TaskController(OptimusService optimusService) {
+        this.optimusService = optimusService;
+    }
+
+    @GetMapping("/today")
+    public List<Task> getTodayTasks(@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken) {
+        return optimusService.getTodayTasks(bearerToken);
+    }
+}
